bdc69a4e151f2ec03496376d2c3ef1e8a4293a6c
Mauricio Molina <mamolina@microsoft.com>
2017-05-05 14:52:47 -0700

    Add csharp sample for endpoint discovery

---
+++ csharp/ScheduledEventsSample/ScheduledEventsDocument.cs
@@ -0,0 +1,35 @@
+﻿/* Copyright 2014 Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
+
+namespace ScheduledEventsSample
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ScheduledEventsDocument
+    {
+        public List<CloudControlEvent> Events { get; set; }
+    }
+
+    public class CloudControlEvent
+    {
+        public string EventId { get; set; }
+        public string EventStatus { get; set; }
+        public string EventType { get; set; }
+        public string ResourceType { get; set; }
+        public List<string> Resources { get; set; }
+        public DateTime? NotBefore { get; set; }
+    }
+}
